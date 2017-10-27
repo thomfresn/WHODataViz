0ef5f339c89b0fe4c0ebf463ee24c9eb4ec8fd4f
Thomas Fresneau <thom.fresneau@gmail.com>
2017-10-27 15:12:47 +0200

    Add comment for logging

--- WHODataViz.WPF/WHODataViz.WPFView/App.xaml.cs
+++ WHODataViz.WPF/WHODataViz.WPFView/App.xaml.cs
@@ -10,6 +10,7 @@ namespace WHODataViz.WPFView
     {
         private void Application_Startup(object sender, System.Windows.StartupEventArgs e)
         {
+            //Load configure for Serilog logger (https://github.com/serilog/serilog)
             Logger logger = new LoggerConfiguration().ReadFrom.AppSettings().CreateLogger();
             Log.Logger = logger;
 
